88132f842dbeed2cfaae47ddf94876a2894874e4
Christian Kvalheim <christkv@gmail.com>
2012-05-24 14:55:24 +0200

    First passing test case

--- test/mongopool_app_tests.erl
+++ test/mongopool_app_tests.erl
@@ -3,12 +3,21 @@
 -include_lib("eunit/include/eunit.hrl").
 
 connection_pool_test() ->
-  application:set_env(mongopool, mongopool_host, "localhost"),
+  % setup the connection
+  application:set_env(mongopool, mongopool_host, "localhost"),  
   application:set_env(mongopool, mongopool_port, 27017),
+  % start the mongopool
   S = application:start(mongopool),
+  % set up a command
   Fun = fun(C) -> mongo_socket:is_master(C) end,
-  %mongopool:execute(Fun)
-  ?debugFmt("connection_pool_test :: ~p~n", [mongopool:execute(Fun)]),
+  % execute the command
+  {ok, {reply, CommandResult}} = mongopool:execute(Fun),
+  % validate that we have a ok for the isMaster and a max bson object field
+  FirstDoc = lists:nth(1, proplists:get_value(docs, CommandResult)),
+  % validate that the command was ok
+  ?assertNot(proplists:get_value(bson:utf8("ok"), FirstDoc) == undefined),
+  ?assertNot(proplists:get_value(bson:utf8("maxBsonObjectSize"), FirstDoc) == undefined),
+  % ?debugFmt("connection_pool_test :: ~p~n", [mongopool:execute(Fun)]),
   % ?assertEqual({ok, pong}, mongopool:execute(Fun)),
   ?assertEqual(1, mongopool:count()),
   application:stop(mongopool),
