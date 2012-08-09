eda2c34652b848d2148c933d7ab04a869c3f8265
Christian Amor Kvalheim <christkv@gmail.com>
2012-08-09 17:16:25 +0200

    Added journal getLastError test

--- test/insert_tests.erl
+++ test/insert_tests.erl
@@ -22,9 +22,26 @@ simple_single_document_insert_safe_test() ->
     % fetch the first object
     ReturnDoc = mongo_socket:find_one(C, <<"erl_test">>, <<"test">>, Document),
     ?assertEqual(1, proplists:get_value(<<"b">>, ReturnDoc))
-    % ?debugFmt("testing :: ~p~n", [Result]),
-    % ?debugFmt("testing :: ~p~n", [proplists:get_value(docs, Result)]),
-    % ?debugFmt("simple_single_document_insert_safe_test 1 :: ~p~n", [ReturnDoc])
-    % fetch value
-    % ?assertEqual(Time, proplists:get_value(<<"a">>, ReturnDoc))
+  end).
+
+simple_single_document_insert_with_journal_set_test() ->
+  % setup the connection
+  application:set_env(mongopool, mongopool_host, "localhost"),
+  application:set_env(mongopool, mongopool_port, 27017),
+  % start the mongopool
+  application:start(mongopool),
+  % insert a simple document not using getlast error
+  Result = mongopool:execute(fun(C) ->
+    % create timestamp
+    {A, B, D} = erlang:now(),
+    Time = bson:utf8(integer_to_list(A) ++ integer_to_list(B) ++ integer_to_list(D)),
+    % create a simple document, prop list
+    Document = [{<<"a">>, Time}, {<<"b">>, 2}],
+    % insert the document with no write concern
+    {reply, Result} = mongo_socket:insert(C, <<"erl_test">>, <<"test">>, Document, [{safe, [{j, true}]}]),
+    ?assertEqual(null, proplists:get_value(<<"err">>, lists:nth(1, proplists:get_value(docs, Result)))),
+    ?assertEqual(0, proplists:get_value(<<"n">>, lists:nth(1, proplists:get_value(docs, Result)))),
+    % fetch the first object
+    ReturnDoc = mongo_socket:find_one(C, <<"erl_test">>, <<"test">>, Document),
+    ?assertEqual(1, proplists:get_value(<<"b">>, ReturnDoc))
   end).
